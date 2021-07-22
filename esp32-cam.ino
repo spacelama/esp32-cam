40d5fd970c326dc5549aefed58c243734ba51f09
Tim Connors <tconnors@rather.puzzling.org>
2021-07-22 23:37:24 +1000

    add first pass at /stream handler.  Doesn't detect client hanging up, and gets stuck in a loop.  Also doesn't handle chunking normal sized images

--- esp32-cam.ino
+++ esp32-cam.ino
@@ -62,6 +62,11 @@ extern Syslog syslog;
 /*         size_t len; */
 /* } jpg_chunking_t; */
 
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
 String CODE_VERSION = "$Revision: 1.9 $";
 WebServer server(80);
 
@@ -89,8 +94,9 @@ void capture_handler(){//httpd_req_t *req){
     // cause a full readout, which might be available immediately, and
     // hopefully calling it again then causes a wait on the next
     // frame, which will then be fully illuminated by the flash
-    fb = esp_camera_fb_get();
-    esp_camera_fb_return(fb);
+//    fb = esp_camera_fb_get();
+//    esp_camera_fb_return(fb);
+    delay(300);
 
     fb = esp_camera_fb_get();
     digitalWrite(4, LOW);
@@ -142,6 +148,7 @@ void capture_handler(){//httpd_req_t *req){
             server.sendContent_P(_buffer, bufferCounter);
             bufferCounter = 0;
         }
+        syslog.log(LOG_INFO, "esp32cam/capture");
     } else {
 //            jpg_chunking_t jchunk = {req, 0};
 //            res = frame2jpg_cb(fb, 80, jpg_encode_stream, &jchunk)?ESP_OK:ESP_FAIL;
@@ -152,6 +159,88 @@ void capture_handler(){//httpd_req_t *req){
 //        int64_t fr_end = esp_timer_get_time();
 //        Serial.printf("JPG: %uB %ums\n", (uint32_t)(fb_len), (uint32_t)((fr_end - fr_start)/1000));
 }
+
+
+void stream_handler() { //httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len = 0;
+    uint8_t * _jpg_buf = NULL;
+    char part_buf[64];
+
+//  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+//  if(res != ESP_OK){
+//    return res;
+//  }
+    server.send(200, _STREAM_CONTENT_TYPE, "");
+
+    while(true){
+        // flash led
+//    pinMode(4, OUTPUT);
+        digitalWrite(4, HIGH);
+        // cause a full readout, which might be available immediately, and
+        // hopefully calling it again then causes a wait on the next
+        // frame, which will then be fully illuminated by the flash
+//    fb = esp_camera_fb_get();
+//    esp_camera_fb_return(fb);
+        delay(300);
+
+        fb = esp_camera_fb_get();
+        digitalWrite(4, LOW);
+        if (!fb) {
+            Serial.println("Camera capture failed");
+            res = ESP_FAIL;
+        } else {
+            if(fb->width > 400){
+                if(fb->format != PIXFORMAT_JPEG){
+                    bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+                    esp_camera_fb_return(fb);
+                    fb = NULL;
+                    if(!jpeg_converted){
+                        Serial.println("JPEG compression failed");
+                        res = ESP_FAIL;
+                    }
+                } else {
+                    _jpg_buf_len = fb->len;
+                    _jpg_buf = fb->buf;
+                }
+            }
+        }
+        /* WiFiClient client = server._server.available(); */
+        /* if (client && !client.connected()) { */
+        /*     res = ESP_FAIL; */
+        /* } */
+        //FIXME: must detect disconnected client
+        if(res == ESP_OK){
+            size_t hlen = snprintf(part_buf, 64, _STREAM_PART, _jpg_buf_len);
+//      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+            server.sendContent_P(part_buf, hlen);
+//    }
+//    if(res == ESP_OK){
+//      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+            server.sendContent_P((const char *)_jpg_buf, _jpg_buf_len);
+//    }
+//    if(res == ESP_OK){
+//      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+            server.sendContent_P(_STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        syslog.log(LOG_INFO, "esp32cam/stream(cont)");
+        if(fb){
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            _jpg_buf = NULL;
+        } else if(_jpg_buf){
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
+        if(res != ESP_OK){
+            break;
+        }
+        //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+    }
+    return;
+}
+
 
 void cmd_handler(){//httpd_req_t *req){
     /* char*  buf; */
@@ -167,19 +256,19 @@ void cmd_handler(){//httpd_req_t *req){
 /* //            return ESP_FAIL; */
 /*             return; */
 /*         } */
-        /* if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) { */
-        /*     if (httpd_query_key_value(buf, "var", variable, sizeof(variable)) == ESP_OK && */
-        /*         httpd_query_key_value(buf, "val", value, sizeof(value)) == ESP_OK) { */
-        /*     } else { */
-        /*         free(buf); */
-        /*         httpd_resp_send_404(req); */
-        /*         return ESP_FAIL; */
-        /*     } */
-        /* } else { */
-        /*     free(buf); */
-        /*     httpd_resp_send_404(req); */
-        /*     return ESP_FAIL; */
-        /* } */
+    /* if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) { */
+    /*     if (httpd_query_key_value(buf, "var", variable, sizeof(variable)) == ESP_OK && */
+    /*         httpd_query_key_value(buf, "val", value, sizeof(value)) == ESP_OK) { */
+    /*     } else { */
+    /*         free(buf); */
+    /*         httpd_resp_send_404(req); */
+    /*         return ESP_FAIL; */
+    /*     } */
+    /* } else { */
+    /*     free(buf); */
+    /*     httpd_resp_send_404(req); */
+    /*     return ESP_FAIL; */
+    /* } */
 /*         free(buf); */
 /*     } else { */
 /* //        httpd_resp_send_404(req); */
@@ -326,6 +415,10 @@ void http_capture() {
     capture_handler();
 }
 
+void http_stream() {
+    stream_handler();
+}
+
 String http_uptime_stub() {
     return "";
 }
@@ -335,6 +428,9 @@ void http_start_stub() {
     server.on("/status", http_status);
     server.on("/control", http_control);
     server.on("/capture", http_capture);
+    server.on("/stream", http_stream);
+
+    server.enableDelay(true);
 };
 
 void setup_stub() {
@@ -342,80 +438,80 @@ void setup_stub() {
 
     Serial.println("esp32cam");
 
-      Serial.setDebugOutput(true);
-  Serial.println();
+    Serial.setDebugOutput(true);
+    Serial.println();
 
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
   
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
+    // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+    //                      for larger pre-allocated frame buffer.
+    if(psramFound()){
+        config.frame_size = FRAMESIZE_UXGA;
+        config.jpeg_quality = 10;
+        config.fb_count = 2;
+    } else {
+        config.frame_size = FRAMESIZE_SVGA;
+        config.jpeg_quality = 12;
+        config.fb_count = 1;
+    }
 
 #if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
+    pinMode(13, INPUT_PULLUP);
+    pinMode(14, INPUT_PULLUP);
 #endif
 
-  // camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
 
-  sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  // Nah, not for the security camera implementation
+    sensor_t * s = esp_camera_sensor_get();
+    // initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID) {
+        s->set_vflip(s, 1); // flip it back
+        s->set_brightness(s, 1); // up the brightness just a bit
+        s->set_saturation(s, -2); // lower the saturation
+    }
+    // drop down frame size for higher initial frame rate
+    // Nah, not for the security camera implementation
 //  s->set_framesize(s, FRAMESIZE_QVGA);
-  s->set_framesize(s, FRAMESIZE_UXGA);
+    s->set_framesize(s, FRAMESIZE_UXGA);
 
 #if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
+    s->set_vflip(s, 1);
+    s->set_hmirror(s, 1);
 #endif
 
-  // led led:
-  //pinMode(33, OUTPUT);
-  //digitalWrite(33, LOW);
-  // flash led
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
-  delay(50);
-  digitalWrite(4, LOW);
+    // led led:
+    //pinMode(33, OUTPUT);
+    //digitalWrite(33, LOW);
+    // flash led
+    pinMode(4, OUTPUT);
+    digitalWrite(4, HIGH);
+    delay(50);
+    digitalWrite(4, LOW);
 }
 
 void loop_stub() {
